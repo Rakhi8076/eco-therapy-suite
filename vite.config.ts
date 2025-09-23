c167323b5f953c862c79e2161265ae05aacc35f5
Rakhi8076 <rakhipratapsingh75505@gmail.com>
2025-09-23 09:26:00 +0530

    changes in host

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+  },
+  preview: {
+    allowedHosts: ["pancharogya.onrender.com"],
   },
   plugins: [
     react(),
@@ -22,3 +24,4 @@ export default defineConfig(({ mode }) => ({
     exclude: ["lucide-react"],
   },
 }));
+
